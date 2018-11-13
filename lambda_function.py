09dcfd69313bb53ce659f3f5234ed45da25e6af2
Rigel Trajano <rigel@gooee.com>
2018-11-13 13:10:45 -0500

    revert status

--- lambda_function.py
+++ lambda_function.py
@@ -56,8 +56,7 @@ CAPABILITY_TO_META = {
         lambda val: {"@type": "IntegralPowerLevel", "value": val}),
     'percentage': ('dim', lambda val: val),
     'connectivity': ('is_online',
-        lambda val: {"value": "OK"})
-        #  lambda val: {"value": "OK" if val else "UNREACHABLE"})
+        lambda val: {"value": "OK" if val else "UNREACHABLE"})
 }
 
 
